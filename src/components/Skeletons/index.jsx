3cfe1e6e9c168adc78a6859b354f8f1ae58a7a6a
Nicolas Sandoli <nicolasandolifeitosa@gmail.com>
2023-09-18 17:46:19 -0300

    primeira versao

---
+++ src/components/Skeletons/index.jsx
@@ -0,0 +1,15 @@
+import {Skeleton} from "@mui/material";
+import React from "react";
+
+export const Skeleton = () => {
+    return (
+        <>
+         <Skeleton variant="rounded" width="100%" height={100} />;
+         <Skeleton variant="rounded" width="100%" height={100} />;
+         <Skeleton variant="rounded" width="100%" height={100} />;
+         <Skeleton variant="rounded" width="100%" height={100} />;
+         <Skeleton variant="rounded" width="100%" height={100} />;
+        </>
+    );
+
+};
